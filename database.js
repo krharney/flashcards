606f5f1a9f85a19ad5bdfb8c5ac5ce7e8d6d336a
Kate <krharney@gmail.com>
2019-10-02 20:28:29 -0400

    Complet server routes for getting all cards from a set, posting a card, and changing the frequency of a card

--- database.js
+++ database.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const autoIncrement = require("mongoose-auto-increment");
+const connection = mongoose.createConnection("mongodb://localhost/mvp");
 
 if (process.env.MLAB_URL === undefined) {
   mongoose.connect("mongodb://localhost/mvp");
+  console.log("connected to db");
+  autoIncrement.initialize(connection);
 } else {
   mongoose.connect(process.env.MLAB_URL);
 }
+
+autoIncrement.initialize(connection);
 
 const flashCardSchema = new Schema({
   set: String,
@@ -14,10 +20,24 @@ const flashCardSchema = new Schema({
   frequency: Number
 });
 
-module.exports.addCard = () => {
-  console.log("added");
+flashCardSchema.plugin(autoIncrement.plugin, "FlashCard");
+
+const FlashCard = mongoose.model("FlashCard", flashCardSchema);
+
+module.exports.addCard = (setName, front, back) => {
+  const card = new FlashCard({
+    set: setName,
+    front: front,
+    back: back,
+    frequency: 3
+  });
+  return card.save();
 };
 
 module.exports.getCards = setName => {
-  console.log("gotten");
+  return FlashCard.find({ set: setName });
+};
+
+module.exports.changeFrequency = (card, frequency) => {
+  return FlashCard.findOneAndUpdate({ _id: card }, { frequency: frequency });
 };
--- server.js
+++ server.js
@@ -8,14 +8,43 @@ const db = require("./database");
 app.use("/", express.static(path.join(__dirname, "dist")));
 app.use(bodyParser.json());
 
+// get request to /cards/:setName specify setName in params and recieve array of card objects
 app.get("/cards/:setName", (req, res) => {
-  db.getCards();
-  res.send(req.params.setName);
+  db.getCards(req.params.setName)
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
+// post req to /cards/:setName specify setName in params and font, back in body
+// frequency auto set to 3 (medium)
 app.post("/cards/:setName", (req, res) => {
-  db.addCard(req.params.setName, req.body.front, req.body.back);
-  res.send(req.params.setName);
+  console.log(req.body);
+  db.addCard(req.params.setName, req.body.front, req.body.back)
+    .then(data => {
+      // console.log(data);
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+// put request to /cards/:id, give frequency in body
+app.put("/cards/:id", (req, res) => {
+  db.changeFrequency(req.params.id, req.body.frequency)
+    .then(data => {
+      res.sendStatus(200);
+    })
+    .catch(err => {
+      res.sendStatus(500);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
